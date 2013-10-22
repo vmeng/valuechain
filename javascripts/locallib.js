9a9f450e7654437cb1efd68ab9356c8f0b06a714
Lulu Ye <yelululiu@gmail.com>
2013-10-22 15:07:01 +0800

    Add more fake data

--- javascripts/locallib.js
+++ javascripts/locallib.js
@@ -1,21 +1,48 @@
 var lib = new localStorageDB('library', localStorage);
-if( lib.isNew() ) {
+// if( lib.isNew() ) {
   try{
     lib.dropTable('X_C_REVENUE_PER_MONTH');
   } catch (ex){
   }
   lib.createTable('X_C_REVENUE_PER_MONTH', ['CUSTOMER_ID', 'YEAR_', 'MONTH_', 'REVENUE_EARNED']);
-  lib.insert('X_C_REVENUE_PER_MONTH', {CUSTOMER_ID: '1004', YEAR_: '2013', MONTH_: '01', REVENUE_EARNED: 1000});
-  lib.insert('X_C_REVENUE_PER_MONTH', {CUSTOMER_ID: '1004', YEAR_: '2013', MONTH_: '02', REVENUE_EARNED: 2000});
+  $.each(CustomerList_new_json.customers,function(index,value){
+    for(var i = 1; i < 10; i++){
+      lib.insert('X_C_REVENUE_PER_MONTH', {CUSTOMER_ID: '' + value.CUST_ACCOUNT_ID, YEAR_: '2013', MONTH_: '0' + i, REVENUE_EARNED: Math.ceil(Math.random()*100000)})
+    }
+  });
   
   try{
     lib.dropTable('QUOTE_HEADER');
   } catch (ex){
   }
   lib.createTable('QUOTE_HEADER', ['QUOTE_CREATE_DATE', 'QUOTE_STATUS', 'QUOTE_NUMBER', 'QUOTE_NAME', 'CUSTOMER_ID', 'CUSTOMER_NAME', 'PARTY_SITE_ID', 'CONTACT_ID', 'CONTACT_NAME']);
-  lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: '2013/6/1', QUOTE_STATUS: 'Draft', QUOTE_NUMBER: '1', QUOTE_NAME: 'quote1', CUSTOMER_ID: '1004', CUSTOMER_NAME:'Hilman and Associates', PARTY_SITE_ID:'1030', CONTACT_ID: '442774', CONTACT_NAME: 'John Rainwater'});
-  lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: '2013/6/3', QUOTE_STATUS: 'Accepted', QUOTE_NUMBER: '2', QUOTE_NAME: 'quote2', CUSTOMER_ID: '1004', CUSTOMER_NAME:'Hilman and Associates', PARTY_SITE_ID:'1030', CONTACT_ID: '442768', CONTACT_NAME: 'John Rainwater'});
-  lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: '2013/6/3', QUOTE_STATUS: 'Draft', QUOTE_NUMBER: '3', QUOTE_NAME: 'quote3', CUSTOMER_ID: '1006', CUSTOMER_NAME:'Computer Service and Rentals', PARTY_SITE_ID:'1090', CONTACT_ID: '442774', CONTACT_NAME: 'John Rainwater'});
+  
+  $.each(CustomerList_new_json.customers,function(index,value){
+    var contacts = [];
+    $.each(ContactList_new_json.contacts, function(contact_index, contact){
+      if('' + value.CUST_ACCOUNT_ID == '' + contact.Customer_account_id){
+        contacts.push(contact);
+      }
+    })
+    var random_day = Math.round(Math.random()*200);
+    var random_contact = Math.floor(Math.random()*(contacts.length-1));
+    if(contacts.length > 0){
+      var num = 1;
+      lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: moment().add('days', -random_day).format('YYYY/MM/DD'), QUOTE_STATUS: 'Draft', QUOTE_NUMBER: '' + num, QUOTE_NAME: 'quote' + num, CUSTOMER_ID: '' + value.CUST_ACCOUNT_ID, CUSTOMER_NAME:value.PARTY_NAME, PARTY_SITE_ID: '' + value.PARTY_SITE_ID, CONTACT_ID: contacts[random_contact].Contact_id, CONTACT_NAME: contacts[random_contact].Contact_First_Name + ' ' + contacts[random_contact].Contact_Last_Name});
+      num++;
+      random_day = Math.round(Math.random()*200);
+      random_contact = Math.floor(Math.random()*(contacts.length-1));
+      lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: moment().add('days', -random_day).format('YYYY/MM/DD'), QUOTE_STATUS: 'Accepted', QUOTE_NUMBER: '' + num, QUOTE_NAME: 'quote' + num, CUSTOMER_ID: '' + value.CUST_ACCOUNT_ID, CUSTOMER_NAME:value.PARTY_NAME, PARTY_SITE_ID: '' + value.PARTY_SITE_ID, CONTACT_ID: contacts[random_contact].Contact_id, CONTACT_NAME: contacts[random_contact].Contact_First_Name + ' ' + contacts[random_contact].Contact_Last_Name});
+      num++;
+      random_day = Math.round(Math.random()*200);
+      random_contact = Math.floor(Math.random()*(contacts.length-1));
+      lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: moment().add('days', -random_day).format('YYYY/MM/DD'), QUOTE_STATUS: 'Accepted', QUOTE_NUMBER: '' + num, QUOTE_NAME: 'quote' + num, CUSTOMER_ID: '' + value.CUST_ACCOUNT_ID, CUSTOMER_NAME:value.PARTY_NAME, PARTY_SITE_ID: '' + value.PARTY_SITE_ID, CONTACT_ID: contacts[random_contact].Contact_id, CONTACT_NAME: contacts[random_contact].Contact_First_Name + ' ' + contacts[random_contact].Contact_Last_Name});
+      num++;
+    }
+  });
+  // lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: '2013/6/1', QUOTE_STATUS: 'Draft', QUOTE_NUMBER: '1', QUOTE_NAME: 'quote1', CUSTOMER_ID: '1004', CUSTOMER_NAME:'Hilman and Associates', PARTY_SITE_ID:'1030', CONTACT_ID: '442774', CONTACT_NAME: 'John Rainwater'});
+  // lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: '2013/6/3', QUOTE_STATUS: 'Accepted', QUOTE_NUMBER: '2', QUOTE_NAME: 'quote2', CUSTOMER_ID: '1004', CUSTOMER_NAME:'Hilman and Associates', PARTY_SITE_ID:'1030', CONTACT_ID: '442768', CONTACT_NAME: 'John Rainwater'});
+  // lib.insert('QUOTE_HEADER', {QUOTE_CREATE_DATE: '2013/6/3', QUOTE_STATUS: 'Draft', QUOTE_NUMBER: '3', QUOTE_NAME: 'quote3', CUSTOMER_ID: '1006', CUSTOMER_NAME:'Computer Service and Rentals', PARTY_SITE_ID:'1090', CONTACT_ID: '442774', CONTACT_NAME: 'John Rainwater'});
   
   try{
     lib.dropTable('QUOTE_DETAIL');
@@ -31,34 +58,55 @@ if( lib.isNew() ) {
   } catch (ex){
   }
   lib.createTable('VISIT_HISTORY', ['CUSTOMER_ID', 'CONTACT_ID', 'CREATION_DATE']);
-  lib.insert('VISIT_HISTORY', {CUSTOMER_ID: '1004', CONTACT_ID: '442774', CREATION_DATE: '2013-06-01'});
-  lib.insert('VISIT_HISTORY', {CUSTOMER_ID: '1004', CONTACT_ID: '442774', CREATION_DATE: '2013-10-22'});
-  lib.insert('VISIT_HISTORY', {CUSTOMER_ID: '1006', CONTACT_ID: '442768', CREATION_DATE: '2013-10-22'});
+  
+  $.each(CustomerList_new_json.customers,function(index,value){
+    var contacts = [];
+    $.each(ContactList_new_json.contacts, function(contact_index, contact){
+      if('' + value.CUST_ACCOUNT_ID == '' + contact.Customer_account_id){
+        contacts.push(contact);
+      }
+    })
+    if(contacts.length > 0){
+      for(var i = 4; i < 9; i++){
+        var random_day = Math.round(Math.random()*200);
+        var random_contact = Math.floor(Math.random()*(contacts.length-1));
+        lib.insert('VISIT_HISTORY', {CUSTOMER_ID: '' + value.CUST_ACCOUNT_ID, CONTACT_ID: contacts[random_contact].Contact_id, CREATION_DATE: moment().add('days', -random_day).format('YYYY-MM-DD')});
+      }
+    }
+  });
   
   try{
     lib.dropTable('MEETING');
   } catch (ex){
   }
   lib.createTable('MEETING', ['MEETING_MINUTES_ID', 'CUSTOMER_ID', 'CONTACT_ID', 'MEETING_MINUTES', 'CREATION_DATE']);
-  lib.insert('MEETING', {MEETING_MINUTES_ID: '1', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Goods Introduction, discussed some points on price.', CREATION_DATE: '2013-06-01 01:20:10'});
-  lib.insert('MEETING', {MEETING_MINUTES_ID: '2', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Goods Introduction, discussed some points on price.', CREATION_DATE: '2013-10-21 02:20:10'});
-  lib.insert('MEETING', {MEETING_MINUTES_ID: '3', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-21 03:20:10'});
-  lib.insert('MEETING', {MEETING_MINUTES_ID: '4', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-22 04:20:10'});
-  lib.insert('MEETING', {MEETING_MINUTES_ID: '5', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-23 05:20:10'});
-  lib.insert('MEETING', {MEETING_MINUTES_ID: '6', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-24 06:20:10'});
+  meeting_mins = [
+    ''
+  ];
+  $.each(lib.query('VISIT_HISTORY'), function(index, value){
+    var time = moment(value.CREATION_DATE).add('m', Math.round(Math.random()*600)).format('YYYY-MM-DD hh:mm:ss');
+    lib.insert('MEETING', {MEETING_MINUTES_ID: '' + index, CUSTOMER_ID: '' + value.CUSTOMER_ID, CONTACT_ID: '' + value.CONTACT_ID, MEETING_MINUTES: 'Introduce the 17-inch-LCD-Monitor and 19-inch-LCD-Monitor, dicussed on the price and quote quantity.', CREATION_DATE: time});
+  });
+  
+  // lib.insert('MEETING', {MEETING_MINUTES_ID: '2', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Goods Introduction, discussed some points on price.', CREATION_DATE: '2013-10-21 02:20:10'});
+  // lib.insert('MEETING', {MEETING_MINUTES_ID: '3', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-21 03:20:10'});
+  // lib.insert('MEETING', {MEETING_MINUTES_ID: '4', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-22 04:20:10'});
+  // lib.insert('MEETING', {MEETING_MINUTES_ID: '5', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-23 05:20:10'});
+  // lib.insert('MEETING', {MEETING_MINUTES_ID: '6', CUSTOMER_ID: '1004', CONTACT_ID: '442774', MEETING_MINUTES: 'Discussion mainly focused on quote.', CREATION_DATE: '2013-10-24 06:20:10'});
   
   try{
     lib.dropTable('INTERESTED_ITEM');
   } catch (ex){
   }
   lib.createTable('INTERESTED_ITEM', ['PITEM_ID', 'CONTACT_ID']);
-  lib.insert('INTERESTED_ITEM', {PITEM_ID: '247', CONTACT_ID: '442774'});
-  lib.insert('INTERESTED_ITEM', {PITEM_ID: '225', CONTACT_ID: '442774'});
-  lib.insert('INTERESTED_ITEM', {PITEM_ID: '199', CONTACT_ID: '442774'});
-  lib.insert('INTERESTED_ITEM', {PITEM_ID: '213', CONTACT_ID: '442774'});
+  $.each(ContactList_new_json.contacts, function(index, value){
+    $.each(ProductList_new_json.products, function(item_index, item){
+      lib.insert('INTERESTED_ITEM', {PITEM_ID: '' + item.Item_Id, CONTACT_ID: value.Contact_id});
+    });
+  });
   
   lib.commit();
-}
+// }
 
 function submitQuote(option){
   var quote_number = moment().format('YYYYMMDDhhmmss');
@@ -157,7 +205,7 @@ function libLoadRevenue(customer_id){
   } else {
     rs = lib.query('X_C_REVENUE_PER_MONTH', {CUSTOMER_ID: '' + customer_id});
   }
-  var totalRevenue = 891236740;
+  var totalRevenue = 0;
   rs.forEach(function(result){
     totalRevenue += result.REVENUE_EARNED;
   })
